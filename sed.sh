3b5ce006a0af3514855ee6238b50886cc4c60d39
ss75710541 <75710541@qq.com>
2019-04-17 19:18:43 +0800

    update sed.sh

--- sed.sh
+++ sed.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-grep 'github.com/ss75710541/3scale-operator' -rl * |xargs sed -i '' 's#github.com/ss75710541/3scale-operator#github.com/ss75710541/3scale-operator#g'
+grep 'github.com/3scale/3scale-operator' -rl *|grep -v sed.sh |xargs sed -i '' 's#github.com/3scale/3scale-operator#github.com/ss75710541/3scale-operator#g'
